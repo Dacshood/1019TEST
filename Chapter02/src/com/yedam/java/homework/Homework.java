fcfcae4bf0ddcd560d7e7f484553793d05dd2542
admin <admin@DESKTOP-T4LE5L6>
2021-10-21 17:21:21 +0900

    c2

---
+++ Chapter02/src/com/yedam/java/homework/Homework.java
@@ -0,0 +1,10 @@
+package com.yedam.java.homework;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test02/src/com/yedam/java/test02/test02.java
@@ -0,0 +1,9 @@
+package com.yedam.java.test02;
+
+public class test02 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
